e4e491186711fcef06eb84e6d298261cf67d5248
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-05-02 22:19:11 +0300

    gmail clone mail deleteion,staring,mail search and dynamic category operations

--- 4-)gmail-clone/js/main.js
+++ 4-)gmail-clone/js/main.js
@@ -1,4 +1,4 @@
-import { renderMails, uiElement } from "./ui.js";
+import { renderMails, uiElement, updateMail } from "./ui.js";
 
 // ! LocaleStorage'daki mail verisi
 const mails = JSON.parse(localStorage.getItem("mails")) || [];
@@ -61,9 +61,13 @@ uiElement.form.addEventListener("submit", (e) => {
       stopOnFocus: true, // Prevents dismissing of toast on hover
       style: {
         background: "linear-gradient(to right, #BF3131 , #7D0A0A)",
+        borderRadius: "10px",
       },
       onClick: function () {}, // Callback after click
     }).showToast();
+
+    // Fonksiyonu durdur
+    return;
   }
 
   // Bir mail objesi oluştur
@@ -103,7 +107,61 @@ uiElement.form.addEventListener("submit", (e) => {
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       background: "linear-gradient(to right, #67AE6E, #328E6E)",
+      borderRadius: "10px",
     },
     onClick: function () {}, // Callback after click
   }).showToast();
+
+  // Mailleri render et
+  renderMails(uiElement.mailsArea, mails);
 });
+
+// Mail alanındaki tıklanmaları izle
+uiElement.mailsArea.addEventListener("click", updateMail);
+
+// Categories Kısmındaki tıklanmalarda çalışacak fonksiyon
+
+uiElement.navigation.addEventListener("click", (e) => {
+  // Tıklanılan elemana en yakın a etiketine eriş
+  const clickedCategory = e.target.closest("a");
+
+  // a etiketlerindeki active classını kaldır
+  const links = uiElement.navigation.querySelectorAll("a");
+  links.forEach((link) => link.classList.remove("active"));
+
+  // Tıklanılan elemana active classını ekle
+  clickedCategory.classList.add("active");
+
+  // Tıklanılan elemanın içerisindeki yazı değerine eriş
+  const categoryName = clickedCategory.innerText;
+
+  // categoryName "Yıldızlananlar" a eşitse mails içerisinden yıldızlanan mailleri al ve ekranda renderla
+  if (categoryName === "Yıldızlananlar") {
+    // Yıldızlanan mailleri al
+    const staredMails = mails.filter((mail) => mail.stared === true);
+
+    // Yıldızlanan mailleri renderla
+    renderMails(uiElement.mailsArea, staredMails);
+  } else {
+    // Yıldızlananlar haricinde bir category seçiliyse tüm mailleri renderla
+    renderMails(uiElement.mailsArea, mails);
+  }
+});
+
+// Search form gönderildiğinde çalışacak fonksiyon
+uiElement.searchForm.addEventListener("submit", (e) => {
+  // Sayfa yenilmesini engelle
+  e.preventDefault();
+  // Input içerisindeki değere eriş
+  const query = e.target[1].value;
+
+  // Aratılan kelimeye göre mails dizisi içerisinde bulunan elemanları bul
+  const filtredMails = mails.filter((i) =>
+    i.receiver.toLowerCase().includes(query.toLowerCase())
+  );
+
+  // Aratılan mailleri renderla
+  renderMails(uiElement.mailsArea, filtredMails);
+});
+
+export { mails };
--- 4-)gmail-clone/js/ui.js
+++ 4-)gmail-clone/js/ui.js
@@ -1,6 +1,7 @@
 // Arayüz Elemanları
 
 import { formatText } from "./helpers.js";
+import { mails } from "./main.js";
 
 const uiElement = {
   hamburgerMenu: document.querySelector("#hamburger-menu"),
@@ -11,17 +12,17 @@ const uiElement = {
   leftAside: document.querySelector(".left-aside"),
   form: document.querySelector("#mail-form"),
   mailsArea: document.querySelector(".mails-area"),
+  searchForm: document.querySelector("#search-form"),
 };
 
 // Mailleri renderlayacak fonksiyon
-
 const renderMails = (outlet, data) => {
   outlet.innerHTML = data
     .map(
-      (mail) => ` <div class="mail">
+      (mail) => ` <div class="mail" data-id="${mail.id}">
             <div class="left">
               <input type="checkbox" />
-              <i class="bi bi-star"></i>
+              <i class="bi bi-star${mail.stared ? "-fill" : ""} "></i>
               <span>${mail.receiver}</span>
             </div>
 
@@ -41,4 +42,73 @@ const renderMails = (outlet, data) => {
     .join(" ");
 };
 
-export { uiElement, renderMails };
+// Maillerde güncelleme yapan fonksiyon
+const updateMail = (e) => {
+  // ! Eğer delete icon'a tıklandıysa
+  if (e.target.classList.contains("bi-trash-fill")) {
+    // Silme işlemi için kullanıcıdan onay al
+    const res = confirm("Silme işlemini onaylıyormusunuz?");
+
+    // Eğer kullanıcı silme işlemini onaylarsa
+    if (res) {
+      // Silinecek mail'e eriş
+      const mail = e.target.closest(".mail");
+      // Mail'in id'sine eriş
+      const mailId = +mail.dataset.id;
+      // Id'si bilinen mail'i mails dizisinden kaldır
+      const filtredMails = mails.filter((mail) => mail.id != mailId);
+
+      // Filtrelenmiş diziyi string'e çevir ve  localstorage'a gönder
+      localStorage.setItem("mails", JSON.stringify(filtredMails));
+
+      // Silinen maili arayüzden'de kaldır
+      mail.remove();
+
+      // Kullanıcıya bildirim gönder
+      Toastify({
+        text: "Mail silindi.",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #F7374F, #FF6363)",
+          borderRadius: "10px",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+  }
+
+  // ! Eğer star iconu'na tıklandıysa
+  if (
+    e.target.classList.contains("bi-star") ||
+    e.target.classList.contains("bi-star-fill")
+  ) {
+    // Yıldızlanacak maile eriş
+    const mail = e.target.closest(".mail");
+    // Mail'in id'sine eriş
+    const mailId = parseInt(mail.dataset.id);
+    // Mails dizisi içerisinden yıldızlanacak elemanı bul
+    const foundedMail = mails.find((i) => i.id == mailId);
+    // Yıldızlanacak elemanın içerisindeki tüm değerleri sabit tut sadece stared değerini mevcut değerin tam tersine çevir
+
+    const updatedMail = { ...foundedMail, stared: !foundedMail.stared };
+
+    // Yıldızlanan elemanın mails dizisindeki index'ini bul
+    const index = mails.findIndex((i) => i.id == mailId);
+
+    // Index'i bilinen elemanı mails dizisi içerisinde güncelle
+    mails[index] = updatedMail;
+
+    // Localstorage'ı güncelle
+    localStorage.setItem("mails", JSON.stringify(mails));
+
+    // Arayüzü renderla
+    renderMails(uiElement.mailsArea, mails);
+  }
+};
+
+export { uiElement, renderMails, updateMail };
